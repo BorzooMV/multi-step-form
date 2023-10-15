402da7c56419d0712f9a2dce4c6ad4d85d35e9dc
Borzoo Moazami <borzoomoazami94@gmail.com>
2023-10-15 19:11:53 +0330

    fix: remove debug context

--- src/components/Layout/Router.tsx
+++ src/components/Layout/Router.tsx
@@ -6,20 +6,16 @@ import SelectAddOns from "../../pages/SelectAddOns";
 import Summery from "../../pages/Summery";
 import ThankYou from "../../pages/ThankYou";
 import NotFound from "../../pages/404";
-import DebugContext from "../DebugContext";
 
 export default function Router() {
   return (
-    <>
-      <DebugContext />
-      <Routes>
-        <Route path="/" element={<Personal />} />
-        <Route path="/select-plan" element={<SelectPlan />} />
-        <Route path="/select-addons" element={<SelectAddOns />} />
-        <Route path="/summary" element={<Summery />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Personal />} />
+      <Route path="/select-plan" element={<SelectPlan />} />
+      <Route path="/select-addons" element={<SelectAddOns />} />
+      <Route path="/summary" element={<Summery />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
