c2c8e4f5c547e1075d2961f1b82e806b08548caa
Borzoo Moazami <borzoo@Borzoos-MacBook-Air.local>
2023-10-16 20:47:28 +0330

    fix: implement sidebar current step indicator functionality

--- src/components/FormWrapper/Sidebar.tsx
+++ src/components/FormWrapper/Sidebar.tsx
@@ -1,8 +1,11 @@
 import StepIndicator from "../StepIndicator";
 import stepsData from "../../data/stepsData.json";
+import useGetStepsFromUrl from "../../hooks/useGetStepsFromUrl";
+
 import "./style.css";
 
 export default function Sidebar() {
+  const { currentStepNumber } = useGetStepsFromUrl();
   return (
     <div className="sidebar rounded-md flex items-start justify-center">
       <div className="flex flex-col gap-4 mt-8">
@@ -10,10 +13,9 @@ export default function Sidebar() {
           return (
             <StepIndicator
               key={index}
-              stepData={{
-                num: index + 1,
-                title: step.indicatorTitle.toUpperCase(),
-              }}
+              num={index + 1}
+              title={step.indicatorTitle.toUpperCase()}
+              isActive={currentStepNumber === index}
             />
           );
         })}
--- src/components/StepIndicator/index.tsx
+++ src/components/StepIndicator/index.tsx
@@ -1,23 +1,39 @@
-import { Avatar, Typography } from "antd";
-const { Text } = Typography;
+import classNames from "classnames";
+import { Typography } from "antd";
+
+import "./style.css";
 
 type StepIndicatorProps = {
-  stepData: {
-    num: number;
-    title?: string;
-  };
+  num: number;
+  title?: string;
+  isActive?: boolean;
 };
 
-export default function StepIndicator({ stepData }: StepIndicatorProps) {
+export default function StepIndicator({
+  num,
+  title,
+  isActive,
+}: StepIndicatorProps) {
+  const { Text } = Typography;
   return (
-    <div className="flex gap-4 items-center">
-      <Avatar size={30} icon={stepData.num} />
+    <div className="flex gap-4 items-center step-indicator">
+      <div
+        className={classNames("step-indicator__background", {
+          "step-indicator__background--active": isActive,
+        })}
+      >
+        <Text className="step-indicator__number">{num}</Text>
+      </div>
       <div className="flex flex-col">
-        <Text className="text-xs text-textLight">Step {stepData.num}</Text>
-        {stepData.title && (
-          <Text className="text-sm font-bold text-textLight">
-            {stepData.title}
-          </Text>
+        <Text
+          className={classNames("text-xs text-textLight", {
+            "text-textDark": isActive,
+          })}
+        >
+          Step {num}
+        </Text>
+        {title && (
+          <Text className="text-sm font-bold text-textLight">{title}</Text>
         )}
       </div>
     </div>
